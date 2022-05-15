3b412692bcd7c295808c32efabf6f49dcf0c0068
ehsan <ehsansaraee6570@yahoo.com>
2022-05-15 16:51:44 +0430

    protecting dashboard from unAuthenticated users

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,9 +1,30 @@
+import { getSession } from 'next-auth/client';
+
 const Dashboard = () => {
-    return(
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
+   return (
+      <div>
+         <h1>Dashboard</h1>
+      </div>
+   );
+};
+
+export const getServerSideProps = async ({ req }) => {
+   const session = await getSession({ req });
+
+   if (!session) {
+      return {
+         redirect: {
+            destination: 'sign_in',
+            permanent: false,
+         },
+      };
+   }
+
+   return {
+      props: {
+         isAuthenticated: session,
+      },
+   };
+};
 
 export default Dashboard;
